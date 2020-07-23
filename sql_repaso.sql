304a1b6bb8e5515fb97033b0312a972113f23830
David Bonilla <david.bonilla625@gmail.com>
2020-07-22 21:29:33 -0600

    Primera y segunda parte terminada

--- sql_repaso.sql
+++ sql_repaso.sql
@@ -1,7 +1,9 @@
 --Repaso
+--PRIMERA PARTE
 USE master;
-
+go
 DROP DATABASE IF EXISTS Control_de_libros_BA181927_GM181938;
+go
 
 CREATE DATABASE Control_de_libros_BA181927_GM181938;
 GO
@@ -46,7 +48,7 @@ values
 	'123-334-456',
 	'Sintaxis básicas de la programación',
 	'204 paginas',
-	200,
+	2000,
 	'ED02'
 ),
 (
@@ -55,7 +57,7 @@ values
 	'345-678-076',
 	'Explicación de las consultas SQL',
 	'156 paginas',
-	1997,
+	2005,
 	'ED03'
 ),
 (
@@ -71,6 +73,15 @@ values
 	'LB04',
 	'Aprender PHP en 30 dias',
 	'234-345-987',
+	'Administración de bases de datos',
+	'150 paginas',
+	2005,
+	'ED01'
+),
+(
+	'LB05',
+	'SQL Server 2008',
+	'789-255-481',
 	'Administración de bases de datos',
 	'150 paginas',
 	2008,
@@ -109,7 +120,86 @@ values
 ('LB01','Estante 1','Disponible'),
 ('LB02','Estante 2','Reservado'),
 ('LB03','Estante 3','Prestado'),
---('LB01','Estante 1','Disponible'),
---('LB05','Estante 5','Disponible'),
+('LB01','Estante 1','Disponible'),
+('LB05','Estante 5','Disponible'),
 ('LB06','Estante 5','Prestado'),
 ('LB06','Estante 5','Disponible');
+go
+
+create table autor(
+codigo varchar(4) primary key,
+nombres varchar(200) not null,
+apellidos varchar(200) not null,
+nacionalidad varchar(200) not null
+);
+go
+
+insert into autor (codigo, nombres, apellidos,nacionalidad) values
+('AU01','JOSE PEDRO','ALVARADO','ESPAÑOLA'),
+('AU02','MARIA TERESA', 'RIVAS','MEXICANO'),
+('AU03','JULIO CARLOS','FERNANDEZ','COLOMBIANO'),
+('AU04','ALEXANDER','RODRIGEZ','MEXICANO'),
+('AU05','JUAN MANUEL','ARTIAGA','COLOMBIANO');
+
+go
+
+
+create table autor_libro(
+cod_libro varchar(4) not null,
+cod_autor varchar (4) not null,
+	constraint fk_cod_libro_libro foreign key (cod_libro) 
+	references libro(codigo),
+
+	constraint fk_cod_autor_autor foreign key (cod_autor) 
+	references autor(codigo)
+);
+go
+insert into autor_libro (cod_libro,cod_autor) values 
+('LB01','AU02'),('LB01','AU04'),
+('LB02','AU01'),('LB03','AU05'),
+('LB03','AU03'),('LB04','AU02'),
+('LB04','AU04');
+go
+--SEGUNDA PARTE
+--A
+select a.*, l.titulo
+from autor a 
+inner join autor_libro al on a.codigo =al.cod_autor
+inner join libro l on al.cod_libro = l.codigo
+group by a.*,
+order by a.nombres desc
+
+go
+--B
+select distinct a.*,e.nombres as 'Nombre editorial' from autor a
+inner join autor_libro al on a.codigo =al.cod_autor
+inner join libro l on al.cod_libro = l.codigo
+inner join editorial e on e.codigo = 'ED02'
+order by a.nombres asc
+--C
+go
+--C
+select l.titulo,
+	(select count (*) from ejemplar e where e.cod_libro = l.codigo) 
+	as ejemplares
+from libro l order by ejemplares desc 
+go
+--D
+select distinct l.titulo from libro l
+inner join ejemplar e on e.cod_libro = l.codigo
+where e.estado = 'Prestado'
+go
+--E
+select * from libro l
+where l.anio_edicion between 2000 and 2007
+order by l.anio_edicion asc
+go
+
+--F
+select e.ubicacion, COUNT (e.cod_libro)
+	as 'Numero libros'	
+	from ejemplar e
+	where e.estado = 'prestado'
+group by e.ubicacion
+go 
+
